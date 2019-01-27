f49901e16a1a349c723f7bc631f3ee712f7e20f4
luojia65 <me@luojia.cc>
2019-01-27 14:53:43 -0500

    mio

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,8 @@
 pub mod discard;
+
+#[cfg(not(feature = "mio-powered"))]
+#[path = "daytime-threads.rs"]
+pub mod daytime;
+#[cfg(feature = "mio-powered")]
+#[path = "daytime-mio.rs"]
 pub mod daytime;
